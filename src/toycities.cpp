0ba4f73224651ee7a85ac4fa1264c8838f18b193
tokudaek <tokudaek@gmail.com>
2019-12-09 18:06:57 -0300

    Compute avg path length

--- src/toycities.cpp
+++ src/toycities.cpp
@@ -43,12 +43,12 @@ typedef struct {
 
 // A utility function to find the vertex with minimum distance value, from
 // the set of vertices not yet included in shortest path tree
-int minDistance(int dist[], bool sptSet[])
+int compute_min_distance(vector<int> dist, vector<bool> sptSet)
 {
 	// Initialize min value
 	int min = INT_MAX, min_index;
 
-	for (int v = 0; v < V; v++)
+	for (int v = 0; v < dist.size(); v++)
 		if (sptSet[v] == false && dist[v] <= min)
 			min = dist[v], min_index = v;
 
@@ -56,53 +56,54 @@ int minDistance(int dist[], bool sptSet[])
 }
 
 // A utility function to print the constructed distance array
-void printSolution(int dist[])
+void printSolution(vector<int> dist)
 {
 	printf("Vertex \t\t Distance from Source\n");
-	for (int i = 0; i < V; i++)
+	for (int i = 0; i < dist.size(); i++)
 		printf("%d \t\t %d\n", i, dist[i]);
 }
 
 // Function that implements Dijkstra's single source shortest path algorithm
 // for a graph represented using adjacency matrix representation
-void dijkstra(int graph[4][4], int src)
+float dijkstra(vector<vector<int>> graph, int src)
 {
-	int dist[V]; // The output array.  dist[i] will hold the shortest
-	// distance from src to i
+	int n = graph.size();
+	vector<int> dist(n, INT_MAX);
+	vector<bool> sptSet(n, false);
 
-	bool sptSet[V]; // sptSet[i] will be true if vertex i is included in shortest
-	// path tree or shortest distance from src to i is finalized
-
-	// Initialize all distances as INFINITE and stpSet[] as false
-	for (int i = 0; i < V; i++)
-		dist[i] = INT_MAX, sptSet[i] = false;
-
-	// Distance of source vertex from itself is always 0
 	dist[src] = 0;
 
-	// Find shortest path for all vertices
-	for (int count = 0; count < V - 1; count++) {
-		// Pick the minimum distance vertex from the set of vertices not
-		// yet processed. u is always equal to src in the first iteration.
-		int u = minDistance(dist, sptSet);
+	for (int count = 0; count < n; count++) {
+		int u = compute_min_distance(dist, sptSet);
 
-		// Mark the picked vertex as processed
 		sptSet[u] = true;
 
-		// Update dist value of the adjacent vertices of the picked vertex.
-		for (int v = 0; v < V; v++)
-
-			// Update dist[v] only if is not in sptSet, there is an edge from
-			// u to v, and total weight of path from src to  v through u is
-			// smaller than current value of dist[v]
+		for (int v = 0; v < n; v++)
 			if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX
 				&& dist[u] + graph[u][v] < dist[v])
 				dist[v] = dist[u] + graph[u][v];
 	}
 
-	// print the constructed distance array
-	printSolution(dist);
+	//printSolution(dist);
+	int acc = 0, nfinite = 0;
+	for (int i = 0; i < dist.size(); i++) {
+		if (dist[i] < INT_MAX) {
+			acc += dist[i];
+			nfinite ++;
+		}
+	}
+	return (float) acc / nfinite;
 }
+
+float compute_average_path_length(vector<vector<int>> graph) {
+	int n = graph.size();
+	float acc = 0;
+
+	for (int i = 0; i < n; i++)
+		acc += dijkstra(graph, i);
+	return acc / n;
+}
+
 //##########################################################
 
 template <class T>
@@ -685,6 +686,9 @@ int main(int, char*[]) {
 			}
 			vector<vector<int>> adj = get_adjmatrix_from_map(blocks, fblocks, edges,
 															 fblockrows, fblockcols);
+			//dijkstra(adj, 0);
+			float x = compute_average_path_length(adj);
+			printf("%f ", x);
 			//print_adj_matrix(adj);
 		}
 	}
